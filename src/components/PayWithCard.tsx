1ae2ab9971c3c1abc189f8cb4f943cadceb9e6fc
Edward Sun <eedwardsun@gmail.com>
2022-05-04 17:11:53 -0700

    New SDK version

--- src/components/PayWithCard.tsx
+++ src/components/PayWithCard.tsx
@@ -1,7 +1,7 @@
-import { PaperSDKError, PaperSDKErrorCode } from "../interfaces/PaperSDKError";
-import React, { useEffect } from "react";
-import { PaymentSuccessResult } from "../interfaces/PaymentSuccessResult";
-import { usePaperSDKContext } from "../Provider";
+import { PaperSDKError, PaperSDKErrorCode } from '../interfaces/PaperSDKError';
+import React, { useEffect } from 'react';
+import { PaymentSuccessResult } from '../interfaces/PaymentSuccessResult';
+import { usePaperSDKContext } from '../Provider';
 
 interface PayWithCardProps {
   checkoutId: string;
@@ -25,36 +25,36 @@ export const PayWithCard: React.FC<PayWithCardProps> = ({
       // if (event.origin !== "https://paper.xyz") return;
 
       const data = event.data;
-      console.log("data is ", data);
+      console.log('data is ', data);
 
-      if (data.eventType === "payWithCardError") {
-        console.error("Error in Paper SDK PayWithCard", data.error);
+      if (data.eventType === 'payWithCardError') {
+        console.error('Error in Paper SDK PayWithCard', data.error);
         if (onError) {
           onError({ code: data.errorCode as PaperSDKErrorCode });
         }
-      } else if (data.eventType === "payWithCardCancel") {
-        console.error("Paper SDK PayWithCard cancelled");
+      } else if (data.eventType === 'payWithCardCancel') {
+        console.error('Paper SDK PayWithCard cancelled');
         if (onCancel) {
           onCancel();
         }
-      } else if (data.eventType === "payWithCardSuccess") {
+      } else if (data.eventType === 'payWithCardSuccess') {
         onSuccess({
           transactionHash: data.transactionHash,
         });
       }
     };
 
-    window.addEventListener("message", handleMessage);
+    window.addEventListener('message', handleMessage);
   }, []);
 
   return (
     <>
       {checkoutId && recipientWalletAddress && chainName && (
-        <>
-          <iframe
-            src={`http://localhost:3000/sdk/v1/pay-with-card?checkoutId=${checkoutId}&recipientWalletAddress=${recipientWalletAddress}&chainName=${chainName}`}
-          ></iframe>
-        </>
+        <iframe
+          src={`http://localhost:3000/sdk/v1/pay-with-card?checkoutId=${checkoutId}&recipientWalletAddress=${recipientWalletAddress}&chainName=${chainName}`}
+          width='100%'
+          height='100%'
+        ></iframe>
       )}
     </>
   );
--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,7 +1,7 @@
-import { PaperSDKError, PaperSDKErrorCode } from "../interfaces/PaperSDKError";
-import { PaperUser } from "../interfaces/PaperUser";
-import React, { useState, useEffect } from "react";
-import { usePaperSDKContext } from "../Provider";
+import { PaperSDKError, PaperSDKErrorCode } from '../interfaces/PaperSDKError';
+import { PaperUser } from '../interfaces/PaperUser';
+import React, { useState, useEffect } from 'react';
+import { usePaperSDKContext } from '../Provider';
 
 interface VerifyEmailProps {
   emailAddress: string;
@@ -27,20 +27,20 @@ export const VerifyEmail: React.FC<VerifyEmailProps> = ({
       // if (event.origin !== "https://paper.xyz") return;
 
       const data = event.data;
-      console.log("data is ", data);
+      console.log('data is ', data);
 
-      if (data.eventType === "verifyEmailEmailVerificationInitiated") {
+      if (data.eventType === 'verifyEmailEmailVerificationInitiated') {
         if (onEmailVerificationInitiated) {
           onEmailVerificationInitiated();
         } else {
           // TODO: Default modal if onEmailVerificationInitiated is not set
         }
-      } else if (data.eventType === "verifyEmailError") {
-        console.error("Error in Paper SDK VerifyEmail", data.error);
+      } else if (data.eventType === 'verifyEmailError') {
+        console.error('Error in Paper SDK VerifyEmail', data.error);
         if (onError) {
           onError({ code: PaperSDKErrorCode.EmailNotVerified });
         }
-      } else if (data.eventType === "verifyEmailSuccess") {
+      } else if (data.eventType === 'verifyEmailSuccess') {
         onSuccess({
           emailAddress: data.emailAddress,
           walletAddress: data.walletAddress,
@@ -48,7 +48,7 @@ export const VerifyEmail: React.FC<VerifyEmailProps> = ({
       }
     };
 
-    window.addEventListener("message", handleMessage);
+    window.addEventListener('message', handleMessage);
   }, []);
 
   const executeVerifyEmail = () => {
@@ -62,12 +62,12 @@ export const VerifyEmail: React.FC<VerifyEmailProps> = ({
           {emailAddress}
           <iframe
             src={`http://localhost:3000/sdk/v1/verify-email?email=${encodeURIComponent(
-              emailAddress
+              emailAddress,
             )}&chainName=${chainName}`}
             style={{
-              width: "0px",
-              height: "0px",
-              visibility: "hidden",
+              width: '0px',
+              height: '0px',
+              visibility: 'hidden',
             }}
           ></iframe>
         </>

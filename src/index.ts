131e4334f397b363538c7df708b30db6f88a5113
Edward Sun <eedwardsun@gmail.com>
2022-05-09 15:40:39 -0700

    Export PaperSDKError types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './components/PaperCheckout';
 export * from './components/PayWithCard';
 export * from './components/CreateWallet';
 export * from './Provider';
+export * from './interfaces/PaperSDKError';

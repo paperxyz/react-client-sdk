389fa20901e4093b7bd2391ee01e8d7ae4f9ce0a
ElasticBottle <winstonyeo99@yahoo.com>
2022-06-28 16:28:46 -0700

    Update pop up format

--- src/lib/utils/popup.ts
+++ src/lib/utils/popup.ts
@@ -3,39 +3,37 @@
  * The caller can close the popup with `popupWindow.close()`.
  * @returns The Window that was popped up
  */
- export function openCenteredPopup({
+export function openCenteredPopup({
+  url,
+  windowName,
+  win,
+  w,
+  h,
+}: {
+  url: string;
+  windowName: string;
+  win: Window & typeof globalThis;
+  w: number;
+  h: number;
+}) {
+  const height = win?.top?.outerHeight || 100;
+  const width = win?.top?.outerWidth || 100;
+  const screenX = win?.top?.screenX || 100;
+  const screenY = win?.top?.screenY || 100;
+  const y = height / 2 + screenY - h / 2;
+  const x = width / 2 + screenX - w / 2;
+  return win.open(
     url,
     windowName,
-    win,
-    w,
-    h,
-  }: {
-    url: string;
-    windowName: string;
-    win: Window & typeof globalThis;
-    w: number;
-    h: number;
-  }) {
-    const height = win?.top?.outerHeight || 100;
-    const width = win?.top?.outerWidth || 100;
-    const screenX = win?.top?.screenX || 100;
-    const screenY = win?.top?.screenY || 100;
-    const y = height / 2 + screenY - h / 2;
-    const x = width / 2 + screenX - w / 2;
-    return win.open(
-      url,
-      windowName,
-      `toolbar=no,
-      location=no,
-      status=no,
-      directories=no,
-      menubar=no,
-      scrollbars=yes,
-      resizable=yes,
-      copyhistory=no,
-      width=${w},
-      height=${h},
-      top=${y},
-      left=${x}`,
-    );
-  }
+    `toolbar=no,
+    location=no,
+    status=no,
+    menubar=no,
+    scrollbars=yes,
+    resizable=yes,
+    width=${w},
+    height=${h},
+    top=${y},
+    left=${x}`,
+  );
+}
